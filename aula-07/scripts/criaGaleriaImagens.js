1351fd67c2a3db7525dd808766f2744f7326f252
Tiago Penha Pedroso <tiago.souza.pedroso@gmail.com>
2022-08-18 00:15:16 -0300

    galeria de imagens com eventos em js

--- aula-07/scripts/criaGaleriaImagens.js
+++ aula-07/scripts/criaGaleriaImagens.js
@@ -1,55 +1,44 @@
-let input1 = document.getElementById("urlImagem1")
-let input2 = document.getElementById("urlImagem2")
-let input3 = document.getElementById("urlImagem3")
+//vars globais
+let inputs = document.querySelectorAll("input[type=text]")
 let divGaleria = document.getElementById("galeria")
+let buttonEnviar = document.querySelector("button")
 
+//lógica principal
+adicionarEventoParaInputs(inputs, "blur")
 
-//TODO: após exibir a imagem deverá ser limpo o input
+buttonEnviar.onclick = enviarUrls
 
-function cliqueiEnviar() {
-    ganhouFocoInput1()
-
-    let img = document.createElement("img")
-    img.src = input1.value
-
-    divGaleria.appendChild(img)
+//funções
+function enviarUrls() {
+    limparGaleria()
+    habilitarInputs(inputs)    
+    adiconarImagensNaGaleria(inputs)    
 }
 
-
-function perdeuFocoInput1() {
-    input1.setAttribute("disabled", true)
+function adicionarEventoParaInputs(listaInputs, nomeEvento) {
+    for (const input of listaInputs) {
+        input.addEventListener(nomeEvento, () => {
+            input.disabled = true
+        })
+    }
 }
 
-function ganhouFocoInput1() {
-    input1.removeAttribute("disabled")
+function habilitarInputs(listaInputs) {
+    for (const input of listaInputs) {
+        input.disabled = false
+    }
 }
 
+function limparGaleria(){
+    divGaleria.innerHTML = ""
+}
 
+function adiconarImagensNaGaleria(listaInputs) {
+    for (const input of listaInputs) {
+        let img = document.createElement("img")
+        img.src = input.value
+        divGaleria.appendChild(img)
 
-
-// //Utilizando virgula no console
-// console.log(
-//     "input1: ",
-//     input1.value,
-//     "input2: ",
-//     input2.value,
-//     "input3: ",
-//     input3.value
-// );
-
-// //com concatenção
-// console.log(
-//     "input1: " + input1.value +
-//     "input2: " + input2.value +
-//     "input3: " + input3.value
-// );
-
-
-// //template String
-// console.log(
-//     `
-//         input1: ${input1.value}, 
-//         input2: ${input2.value}, 
-//         input3: ${input3.value}
-//     `
-// );
+        input.value = ""
+    }
+}
